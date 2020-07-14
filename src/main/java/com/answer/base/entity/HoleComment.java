cff4d9fb253999e5e89c74512b7a48efd6933478
answer9527 <516994147.com>
2020-07-14 08:55:40 +0800

    home 0714001

--- src/main/java/com/answer/base/entity/HoleComment.java
+++ src/main/java/com/answer/base/entity/HoleComment.java
@@ -12,7 +12,7 @@ public class HoleComment {
     private Integer id;
     private Integer uid;
     private String userName;
-    private String avatarUrl;
+    private String userAvatar;
     private Integer uid_r;
     private String userName_r;
     private String avatarUrl_r;
